e9c1e448fd339b95fdb45a8047397d48c6a02338
[2기-1반] 김진경 <kimradium@naver.com>
2021-08-04 15:36:10 +0900

    2번쨰 커밋: 마운트, 언마운트 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
 
 function TestOne() {
+
+  useEffect(() => {
+    console.log("1번째 컴포넌트 마운트 완료")
+
+    return () => {
+      console.log("1번째 컴포넌트 언마운트")
+    }
+
+  },[]);
+
   return(
     <div style={{ width: "300px", height: "100px", background: "skyblue" }}>
       1번째 컴포넌트 입니다.
@@ -10,6 +21,16 @@ function TestOne() {
 }
 
 function TestTwo() {
+
+  useEffect(() => {
+    console.log("2번째 컴포넌트 마운트 완료")
+
+    return () => {
+      console.log("2번째 컴포넌트 언마운트")
+    }
+
+  },[]);
+
   return(
     <div style={{ width: "300px", height: "100px", background: "Khaki" }}>
       2번째 컴포넌트 입니다.
@@ -18,11 +39,13 @@ function TestTwo() {
 }
 
 function Select() {
+
+  const [isToggle, setToggle] = useState(false);
+
   return(
     <div>
-      <input type="button" value="컴포넌트 변경" />
-      <TestOne />
-      <TestTwo />
+      <input type="button" value="컴포넌트 변경" onClick={() => setToggle(!isToggle)}/>
+        {isToggle === false ? <TestOne /> : <TestTwo />}
     </div>
   )
 }
