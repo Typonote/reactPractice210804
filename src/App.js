e78d9d8576a68612e214901997aa3b3810568896
[2기-1반] 김진경 <kimradium@naver.com>
2021-08-04 15:39:18 +0900

    3번쨰: 주석작성

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react'
 
+
+// 마운트 됐을 때
+// 마운트는 컴포넌트가 첫 렌더링 시에만 실행되는 이벤트입니다.
+// 클래스 컴포넌트에서는 componentDidMount() 로 사용하며
+// 함수 컴포넌트에서는 useEffect를 사용해서 구현할 수 있습니다.
+
+
+// 언마운트 됐을 때 (컴포넌트가 제거 됐을 때)
+// 마운트의 반대 개념으로 컴포넌트가 사라지는 경우 이벤트가 발생합니다.
+// 클래스 컴포넌트에서는 componentWillUnmount() 를 사용하며 
+// 함수 컴포넌트에서는 useEffect 내 함수에서 return 부분이 해당 기능을 담당합니다.
+
+
+
+
 function TestOne() {
 
   useEffect(() => {
